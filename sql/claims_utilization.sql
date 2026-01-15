853ea1c375bcffbff008177c0c8d0ad61d6a5330
Aishwarya055 <aishwarya143639@gmail.com>
2026-01-15 15:04:00 -0500

    Delete sql directory

--- sql/claims_utilization.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    c.plan_type,
-    c.claim_type,
-    COUNT(c.claim_id) AS total_claims,
-    SUM(c.procedure_cost) AS total_cost
-FROM claims c
-GROUP BY c.plan_type, c.claim_type
-ORDER BY total_cost DESC;
--- sql/sql/provider_performance.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    p.provider_name,
-    COUNT(c.claim_id) AS claims_processed,
-    AVG(c.procedure_cost) AS avg_claim_cost
-FROM claims c
-JOIN providers p ON c.provider_id = p.provider_id
-GROUP BY p.provider_name;
